49f3890669863ca7f1067b7570e4cf924cd370af
Robert Olbrychowski <47324286+Robert-Sawyer@users.noreply.github.com>
2023-08-16 21:31:46 +0200

    Add fonts from Google

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,8 @@
-import styles from './page.module.css'
+import React from "react";
 
 export default function Home() {
   return (
-    <main className={styles.main}>
+    <main className='main'>
       <div>Hello World</div>
     </main>
   )
